9c1e998a80bfe5d7acb663005300e9943bf32242
kwonjun12 <gwonjunyeong78@gmail.com>
2026-04-25 21:59:38 +0900

    [Gold IV] Title: 트리의 지름, Time: 4 ms, Memory: 3036 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#define MAX 10001
+using namespace std;
+
+typedef struct {
+    int l, w;
+} tree;
+
+int n, fn, ans;
+int visited[MAX];
+vector<tree>nw[MAX];
+
+void dfs(int n, int m) {
+    visited[n-1] = 1;
+    if (m > ans) {
+        ans = m;
+        fn = n;
+    }
+    for (int i = 0; i < nw[n].size(); i++) {
+        tree nn = nw[n][i];
+        if (!visited[nn.l-1]) {
+            dfs(nn.l, m+nn.w);
+        }
+    }
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cin >> n;
+    int temp1, temp2, temp3;
+    for (int i = 0; i < n-1; i++) {
+        cin >> temp1 >> temp2 >> temp3;
+        nw[temp1].push_back({temp2, temp3});
+        nw[temp2].push_back({temp1, temp3});
+    }
+    dfs(1, 0);
+    ans = 0;
+    memset(visited, 0, sizeof(int)*n);
+    dfs(fn, 0);
+    cout << ans;
+}
