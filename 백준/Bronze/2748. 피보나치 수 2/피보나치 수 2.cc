cb3cdfad06ab7f452fdce12903c6010a055ff607
kwonjun12 <gwonjunyeong78@gmail.com>
2026-04-27 21:55:25 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+long long dp[91] = {0, 1, 1, 2}; 
+
+int main(void){
+   int n;
+   scanf("%d", &n);
+   for(int i = 4; i <= n; i++){
+       dp[i] = dp[i - 1] + dp[i - 2];
+   } 
+   printf("%lld", dp[n]); 
+}
