13903630b2efd4ab5fab5bdd6f8390e69493525f
kwonjun12 <gwonjunyeong78@gmail.com>
2026-04-27 22:08:28 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int n,m;
+int arr[10];
+int ans[10];
+bool v[10];
+
+void dfs(int l){
+    if(l == m){
+       for(int i = 0; i < m; i++){
+           cout << ans[i] << " ";
+       }
+       cout << "\n";
+        return;
+    }
+    int la = 0;
+    for(int i = 0; i < n; i++){
+        if(v[i] || arr[i] == la) continue;
+        v[i] = 1;
+        ans[l] = la = arr[i];
+        dfs(l+1);
+        v[i] = 0;
+    }
+}
+
+int main(void){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+       cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++){
+        ans[0] = arr[i];
+        sort(arr, arr + n);
+        dfs(0);
+        return 0;
+    }
+}
