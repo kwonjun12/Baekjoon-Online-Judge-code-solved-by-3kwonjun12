be656f6c65c1a3b5e03a52798cae3508fdd81138
kwonjun12 <gwonjunyeong78@gmail.com>
2026-04-27 21:54:19 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -3,19 +3,19 @@
 
 using namespace std;
 
-int main(void){
+int main(void) {
     int n;
     scanf("%d", &n);
     int s[n+1];
     int dp[n+1];
-    for(int i = 1; i <= n; i++){
+    for (int i = 1; i <= n; i++) {
         scanf("%d", &s[i]);
     }
     dp[1] = s[1];
-    dp[2] = dp[1] + s[2];
-    dp[3] = max(s[1]+s[3], s[2]+s[3]);
-    for(int i = 4; i <= n; i++){
-        dp[i] = max(dp[i-2]+s[i],dp[i-3]+s[i-1]+s[i]);
+    dp[2] = s[1] + s[2];
+    dp[3] = max(s[1] + s[3], s[2] + s[3]);
+    for (int i = 4; i <= n; i++) {
+        dp[i] = max(dp[i - 2], dp[i - 3] + s[i - 1]) + s[i];
     }
     printf("%d", dp[n]);
 }
