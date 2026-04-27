c5532bcb2d333e46dff4ad4e1bb2239fe4347c2b
kwonjun12 <gwonjunyeong78@gmail.com>
2026-04-27 21:51:34 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 1188 KB -BaekjoonHub

---
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <algorithm>
+
+using namespace std;
+
+int arr[10001];
+int dp[10001];
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    dp[1] = arr[1];
+    dp[2] = arr[1] + arr[2];
+    for (int i = 3; i <= n; i++) {
+        dp[i] = max(dp[i - 1], max(dp[i - 2] + arr[i], dp[i - 3] + arr[i - 1] + arr[i]));
+    }
+    printf("%d", dp[n]);
+}
